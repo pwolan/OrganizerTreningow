a78511e38d34af5f9ce0c3533c1633b5f37574be
Mateusz Zając <matzayonc@gmail.com>
2023-04-13 13:35:13 +0200

    actual updating

--- user/user.py
+++ user/user.py
@@ -2,6 +2,18 @@ from flask import Blueprint, render_template
 import flask
 import json
 
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import pytz
+import datetime
+
+from credentials_required import credentials_required
+
+
+API_SERVICE_NAME = 'calendar'
+API_VERSION = 'v3'
+
 userRoutes = Blueprint('user', __name__)
 
 
@@ -20,10 +32,42 @@ def edit():
     return render_template("edit.html", eventId=eventId)
 
 @userRoutes.post('/edited')
-def edited():
+@credentials_required
+def edited(credentials):
+    service = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
     eventId = flask.request.form["eventId"]
-    print(eventId)
+    if not eventId:
+        return "No event ID provided"
+
+    event = service.events().get(calendarId='primary', eventId=eventId).execute()
 
 
-    return json.dumps(eventId)
+    if flask.request.form["title"]:
+        event["summary"] = flask.request.form["title"]
+
+    duration = None
+
+    # update start time
+    if flask.request.form["time"]:
+        old_start = datetime.datetime.fromisoformat(event["start"]['dateTime'])
+        old_end = datetime.datetime.fromisoformat(event["end"]['dateTime'])
+        duration = old_end - old_start
+        print('writing: ', flask.request.form['time'])
+        event["start"]['dateTime'] = flask.request.form['time'] + ':00'
+
+
+    # update duration if changed
+    if flask.request.form['duration']:
+        duration = datetime.timedelta(minutes=int(flask.request.form["duration"]))
+
+    # update end time based on start time and duration
+    if flask.request.form['time'] or flask.request.form['duration']:
+        start = datetime.datetime.fromisoformat(event["start"]['dateTime'])
+        event["end"]['dateTime'] = (start + duration).isoformat()
+
+
+    updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+
     return render_template("edit.html")
